81c3a0e53d1f686edfda91f556d60bff693acf80
vedantBCodes <bichkundevedant97@gmail.com>
2023-03-10 13:32:41 -0800

    new progeam added

---
+++ maximumTripletProduct.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,arr[30],i,j,temp,prod=1;
+    cout<<"Enter the size of array:";
+    cin>>x;
+    cout<<"Enter "<<x<<" array elements:";
+    for(i=0;i<x;i++)
+    cin>>arr[i];
+    for(i=0;i<x;i++)
+    {
+        for(j=i+1;j<x;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(i=x-1;i>x-4;i--)
+    {
+        prod=prod*arr[i];
+    }
+    cout<<"The maximum possible triplet product is:"<<prod;
+}
